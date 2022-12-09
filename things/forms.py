22662f4e59695d6213f008a52729dade9fc0a33e
Isaac Leegit config --global user.name Isaac <shun.s.lee@kcl.ac.uk>
2022-12-09 15:29:29 +0000

    things form

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,22 @@
+from django import forms 
+from .models import Thing
+
 """Forms of the project."""
 
-# Create your forms here.
+class ThingForm(forms.ModelForm):
+    name = forms.CharField(label= "name")
+    description = forms.Textarea(label="description")
+    quantity = forms.NumberInput(
+        label = "quantity"
+    )
+    def save(self): 
+                super().save(commit=False) 
+                thing =  Thing.objects.create_thing(
+                        self.cleaned_data.get ('name'),
+                        first_name = self.cleaned_data.get ('description'),
+                        last_name = self.cleaned_data.get ('quantity'),
+                )
+                return thing
+        
+        
+                
--- things/views.py
+++ things/views.py
@@ -1,4 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            thing = form.save()
+            return redirect ('home.html')
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
